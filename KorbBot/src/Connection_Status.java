ace4f2cb6a4f4ca79b9bce36e1b53d71b87f9157
artisiole <43116512+artisiole@users.noreply.github.com>
2020-05-25 19:44:06 -0500

    whoops 4.0

--- KorbBot/src/Connection_Status.java
+++
@@ -1,8 +0,0 @@
-public class Connection_Status {
-    public static void main(String[] args) throws InterruptedException {
-        while(true){
-            System.out.println("Connected!");
-            Thread.sleep(3600000);
-        }
-    }
-}
